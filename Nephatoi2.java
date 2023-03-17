093191934f57152a404f43f86ea8ca76c34a2b8f
Ongtrumxadoan <103736587+Ongtrumxadoan@users.noreply.github.com>
2023-03-17 21:43:57 +0700

    Add files via upload

---
+++ Nephatoi2.java
@@ -0,0 +1,242 @@
+MENU--------------
+Vui long chon chuc nang ------------
+---1.quan ly san pham trong kho Tivi Smart---
+---2.quan ly san pham trong kho Tivi Internet---
+---0.thoat chuong trinh---
+2
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+1
+Nhap ma tivi: 
+01
+Nhap ten tivi: 
+ly duc phat tv
+Nhap xuat xu tivi: 
+thai lan 
+Nhap namsx tivi: 
+1900
+Nhap so hang trong kho tivi: 
+1200
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+1
+Nhap ma tivi: 
+02
+Nhap ten tivi: 
+vu trong phuong tv
+Nhap xuat xu tivi: 
+indonesia
+Nhap namsx tivi: 
+1902
+Nhap so hang trong kho tivi: 
+1200
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+2
+TiviIT ma=01, ten=ly duc phat tv, namSx=1900, xuatXu=thai lan , hTrongkho=1200.0
+TiviIT ma=02, ten=vu trong phuong tv, namSx=1902, xuatXu=indonesia, hTrongkho=1200.0
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+3
+so luong loai san pham trong kho :2
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+4
+Nhap ten tivi: 
+01
+ket qua tim kiem
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+4
+Nhap ten tivi: 
+ly duc phat tv
+ket qua tim kiem
+TiviIT ma=01, ten=ly duc phat tv, namSx=1900, xuatXu=thai lan , hTrongkho=1200.0
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+5
+TiviIT ma=01, ten=ly duc phat tv, namSx=1900, xuatXu=thai lan , hTrongkho=1200.0
+TiviIT ma=02, ten=vu trong phuong tv, namSx=1902, xuatXu=indonesia, hTrongkho=1200.0
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+6
+nhap vao ma can sua : 
+01
+ nhap du lieu moi: 
+Nhap ma tivi: 
+03
+Nhap ten tivi: 
+duc nguyen tv
+Nhap xuat xu tivi: 
+1900
+Nhap namsx tivi: 
+1900
+Nhap so hang trong kho tivi: 
+120000
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+7
+nhap vao ma can xoa : 
+02
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+2
+TiviIT ma=03, ten=duc nguyen tv, namSx=1900, xuatXu=1900, hTrongkho=120000.0
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+8
+ nhap ten file 
+03
+luu file thanh cong
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+9
+ nhap ten file muon doc 
+duc nguyen tv
+TiviIT ma=03, ten=duc nguyen tv, namSx=1900, xuatXu=1900, hTrongkho=120000.0
+MENU--------------
+KHO Tivi Internet--------------
+---Vui long chon chuc nang ------------
+---1.them san pham trong kho---
+---2.in danh sach san pham trong kho ra man hinh---
+---3.dem loai san pham trong kho---
+---4.tim kiem ten san pham trong kho---
+---5.sap xep san pham theo so luong giam dan---
+---6.Sua thong tin san pham---
+---7.Xoa thong tin san pham---
+---8.Luu file ---
+---9.doc file ---
+---0.thoat chuong trinh---
+
